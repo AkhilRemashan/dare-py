2e2eea2f8abd6bebeecb9d36bb5a788a9679500a
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-03-25 14:08:23 +0530

    py_1_02

---
+++ db_refresh/py/1/basics/06 - user_input.py
@@ -0,0 +1,2 @@
+full_name = input("Enter your name : ")
+print("Hello " + full_name + "!")
---
+++ h_cs50/flask_1/first_flask.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # return "hello world!"
+
+    name = request.args.get("name", "world")
+    return render_template("index.html", name=name)
