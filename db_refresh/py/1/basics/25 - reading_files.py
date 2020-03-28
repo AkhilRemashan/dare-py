beb846dbcc406b3af068888aa60abc0b536a540a
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-03-28 05:25:27 -0700

    updated 2803

---
+++ db_refresh/py/1/basics/25 - reading_files.py
@@ -0,0 +1,14 @@
+
+manager_details = open("sample.txt", "r")
+
+# Making file readable
+# print(manager_details.readable())
+
+# Reading file
+# print(manager_details.read())
+
+# Reading lines making it into an array
+print(manager_details.readlines())
+
+manager_details.close()
+
