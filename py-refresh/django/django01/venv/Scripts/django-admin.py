59c1d625938ad832b266a325aaf517aa526b50c5
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-04-13 21:29:08 +0530

    18.0

---
+++ py-refresh/django/django01/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\py_things\dare-py\py-refresh\django\django01\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
