93f61c2f6d55dd00a854da0a2b909f4a57bbc2e1
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-04-26 22:16:25 +0530

    nickscode

---
+++ py-refresh/django/cs50/nickwong/rango/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\py_things\dare-py\py-refresh\django\cs50\nickwong\rango\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
