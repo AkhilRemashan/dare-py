5fffefc6e7df62c988bed81a53306fadbc04ec24
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-07-16 08:00:12 -0700

    0:2610

--- postgres-tuts/flask/app.py
+++ postgres-tuts/flask/app.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def index_page():
+    return render_template('index.html')
+#
+@app.route('/multi-line')
 def hello_world():
-    return 'Hello, World!'
+    return '''
+        <h3>multiline index</h3>
+        <p>ML lorem ipsum</p>
+    '''
+#
+@app.route('/login/<string:username>')
+def w_username(username):
+    return 'welcome, ' + username
+#
+@app.route('/only-get', methods=['GET'])
+def only_get():
+    return 'only get'
+#
+@app.route('/only-post', methods=['POST'])
+def only_post():
+    return 'only post'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
