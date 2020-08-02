308b355537c051e3888dfbde965775873a518120
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-08-03 01:11:45 +0530

    blog-3

--- py/flask/blog/app.py
+++ py/flask/blog/app.py
@@ -73,6 +73,20 @@ def edit(id):
         return redirect('/posts')
     else:
         return render_template('edit.html', post=post)
+
+@app.route('/posts/new', methods=['GET', 'POST'])
+def new_post():
+    
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('new_post.html')
+
 #
 @app.route('/multi-line')
 def hello_world():
